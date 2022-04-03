b2f53dae571f14c680760f783c5dbe5c7c3e6309
Duy Truong <truongduy.fu@gmail.com>
2022-04-03 16:06:52 +0700

    first commit

---
+++ server/data/products.js
@@ -0,0 +1,87 @@
+const products = [
+	{
+		name: 'iPhone 13 Pro Max Black 256GB',
+		image: '/images/13_pro_black.png',
+		description: 'asd',
+		brand: 'Apple',
+		category: 'Electronics',
+		price: 33000000,
+		countInStock: 3,
+		rating: 5,
+		numReviews: 3,
+	},
+	{
+		name: 'iPhone 13 Pro Max Cream 256GB',
+		image: '/images/13_pro_cream.png',
+		description:
+			'iPhone 13 Pro Max 256GB được đánh giá là một trong những dòng iPhone có khả năng chụp ảnh siêu ấn tượng cùng với camera góc siêu rộng mang đến khả năng chụp ảnh thiếu sáng một cách đặc biệt. Không những thế, mà dòng iPhone này còn được đánh giá là có dung lượng bộ nhớ tốt, đáp ứng được đầy đủ các công việc hằng ngày. Xem thêm thông tin iPhone 13 Pro Max 512GB thiết kế đẳng cấp, mang lại trải nghiệm vượt trội cho người dùng',
+		brand: 'Apple',
+		category: 'Electronics',
+		price: 33000000,
+		countInStock: 3,
+		rating: 5,
+		numReviews: 3,
+	},
+	{
+		name: 'iPhone 13 Pro Max Green 256GB',
+		image: '/images/13_pro_green.png',
+		description:
+			'iPhone 13 Pro Max 256GB được đánh giá là một trong những dòng iPhone có khả năng chụp ảnh siêu ấn tượng cùng với camera góc siêu rộng mang đến khả năng chụp ảnh thiếu sáng một cách đặc biệt. Không những thế, mà dòng iPhone này còn được đánh giá là có dung lượng bộ nhớ tốt, đáp ứng được đầy đủ các công việc hằng ngày. Xem thêm thông tin iPhone 13 Pro Max 512GB thiết kế đẳng cấp, mang lại trải nghiệm vượt trội cho người dùng',
+		brand: 'Apple',
+		category: 'Electronics',
+		price: 33000000,
+		countInStock: 3,
+		rating: 5,
+		numReviews: 3,
+	},
+	{
+		name: 'Google Pixel 6 Pro 5G Black 12GB|128GB',
+		image: '/images/gg6_black.png',
+		description:
+			'Đánh giá Google Pixel 6 Pro vừa ra mắt của Google cho thấy đây là đối thủ đáng gờm của Samsung và iPhone. Điện thoại nổi bật với sự xuất hiện của chip xử lý Tensor, camera chuyên nghiệp, màn hình hiển thị chất lượng, pin lớn...hứa hẹn mang đến trải nghiệm tuyệt vời.',
+		brand: 'Google',
+		category: 'Electronics',
+		price: 26990000,
+		countInStock: 3,
+		rating: 5,
+		numReviews: 3,
+	},
+	{
+		name: 'Google Pixel 6 Pro 5G White 12GB|128GB',
+		image: '/images/gg6_white.png',
+		description:
+			'Đánh giá Google Pixel 6 Pro vừa ra mắt của Google cho thấy đây là đối thủ đáng gờm của Samsung và iPhone. Điện thoại nổi bật với sự xuất hiện của chip xử lý Tensor, camera chuyên nghiệp, màn hình hiển thị chất lượng, pin lớn...hứa hẹn mang đến trải nghiệm tuyệt vời.',
+		brand: 'Google',
+		category: 'Electronics',
+		price: 26990000,
+		countInStock: 3,
+		rating: 5,
+		numReviews: 3,
+	},
+	{
+		name: 'Google Pixel 6 Pro 5G Yellow 12GB|128GB',
+		image: '/images/gg6_yellow.png',
+		description:
+			'Đánh giá Google Pixel 6 Pro vừa ra mắt của Google cho thấy đây là đối thủ đáng gờm của Samsung và iPhone. Điện thoại nổi bật với sự xuất hiện của chip xử lý Tensor, camera chuyên nghiệp, màn hình hiển thị chất lượng, pin lớn...hứa hẹn mang đến trải nghiệm tuyệt vời.',
+		brand: 'Google',
+		category: 'Electronics',
+		price: 26990000,
+		countInStock: 3,
+		rating: 5,
+		numReviews: 3,
+	},
+	{
+		name: 'Google Pixel 6 Pro 5G Red 12GB|128GB',
+		image: '/images/gg6_red.png',
+		description:
+			'Đánh giá Google Pixel 6 Pro vừa ra mắt của Google cho thấy đây là đối thủ đáng gờm của Samsung và iPhone. Điện thoại nổi bật với sự xuất hiện của chip xử lý Tensor, camera chuyên nghiệp, màn hình hiển thị chất lượng, pin lớn...hứa hẹn mang đến trải nghiệm tuyệt vời.',
+		brand: 'Google',
+		category: 'Electronics',
+		price: 26990000,
+		countInStock: 3,
+		rating: 5,
+		numReviews: 3,
+	},
+];
+
+export default products;
---
+++ server/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from 'bcryptjs';
+
+const users = [
+	{
+		name: 'Admin',
+		email: 'admin@gmail.com',
+		password: bcrypt.hashSync('123456', 10),
+		isAdmin: true,
+	},
+	{
+		name: 'Duy Truong',
+		email: 'duycute@gmail.com',
+		password: bcrypt.hashSync('123456', 10),
+	},
+	{
+		name: 'Truong Duy',
+		email: 'duydeptrai@gmail.com',
+		password: bcrypt.hashSync('123456', 10),
+	},
+];
+
+export default users;
