ef3671c59b91c19eb0ec44f26f2ec11b6fa6fb6b
Duy Truong <truongduy.fu@gmail.com>
2022-04-10 00:44:03 +0700

    upadte

--- client/src/screens/OrderListScreen.js
+++ client/src/screens/OrderListScreen.js
@@ -7,66 +7,66 @@ import Loader from '../components/Loader';
 import { listOrders } from '../actions/orderActions';
 
 const OrderListScreen = ({ history }) => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const orderList = useSelector(state => state.orderList);
-	const { loading, error, orders } = orderList;
+  const orderList = useSelector((state) => state.orderList);
+  const { loading, error, orders } = orderList;
 
-	const userLogin = useSelector(state => state.userLogin);
-	const { userInfo } = userLogin;
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
 
-	useEffect(() => {
-		if (userInfo && userInfo.isAdmin) {
-			dispatch(listOrders());
-		} else {
-			history.push('/login');
-		}
-	}, [dispatch, history, userInfo]);
+  useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listOrders());
+    } else {
+      history.push('/login');
+    }
+  }, [dispatch, history, userInfo]);
 
-	return (
-		<>
-			<h1>Orders</h1>
-			{loading ? (
-				<Loader />
-			) : error ? (
-				<Message variant='danger'>{error}</Message>
-			) : (
-				<Table striped bordered hover responsive className='table-sm'>
-					<thead>
-						<tr>
-							<th>Mã đơn</th>
-							<th>Người dùng</th>
-							<th>Ngày đặt</th>
-							<th>Tổng</th>
-							<th></th>
-						</tr>
-					</thead>
-					<tbody>
-						{orders.map(order => (
-							<tr key={order._id}>
-								<td>{order._id}</td>
-								<td>{order.user && order.user.name}</td>
-								<td>{order.createdAt.substring(0, 10)}</td>
-								<td>
-									{order.totalPrice
-										.toString()
-										.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-									vnđ
-								</td>
-								<td>
-									<LinkContainer to={`/order/${order._id}`}>
-										<Button variant='light' className='btn-sm'>
-											Chi tiết
-										</Button>
-									</LinkContainer>
-								</td>
-							</tr>
-						))}
-					</tbody>
-				</Table>
-			)}
-		</>
-	);
+  return (
+    <>
+      <h1>Đơn hàng</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Table className="table-sm" striped bordered hover responsive>
+          <thead>
+            <tr>
+              <th>Mã đơn</th>
+              <th>Người dùng</th>
+              <th>Ngày đặt</th>
+              <th>Tổng</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <tr key={order._id}>
+                <td>{order._id}</td>
+                <td>{order.user && order.user.name}</td>
+                <td>{order.createdAt.substring(0, 10)}</td>
+                <td>
+                  {order.totalPrice
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                  vnđ
+                </td>
+                <td>
+                  <LinkContainer to={`/order/${order._id}`}>
+                    <Button variant="light" className="btn-sm">
+                      Chi tiết
+                    </Button>
+                  </LinkContainer>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  );
 };
 
 export default OrderListScreen;
